51eb7a5cacc04bd4db6bea3b01cb8b5eb6c9bb26
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-04 00:32:46 +0800

    commit

--- dev/demo/api/onDemo.js
+++ dev/demo/api/onDemo.js
@@ -1,1 +1,1 @@
-async function demo(params, context) { return { "statusCode": 200, "body": "Hello from auto Lambda!" }; } async function onDemo_default(event) { return await demo(event, __buildContext(event)); } export {  onDemo_default as default };
+async function demo(params, context) { return { "statusCode": 200, "body": "222Hello from auto Lambda!" }; } async function onDemo_default(event) { return await demo(event, __buildContext(event)); } export {  onDemo_default as default };
