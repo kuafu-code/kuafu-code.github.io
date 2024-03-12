431747bbc637cd1601412c7077c0b617f0806c25
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-12 15:00:08 +0800

    commit

--- modules/demo/dev/plugin/myapps.js
+++ modules/demo/dev/plugin/myapps.js
@@ -2,7 +2,7 @@
 async function myapps_default() {
 }
 
-// build/demo/api/myapps.ts
+// build/modules/demo/plugin/myapps.ts
 async function myapps_default2(event) {
   return await myapps_default(event, __buildContext(event));
 }
