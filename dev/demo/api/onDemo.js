a2082d63050a958ac248e18df9034ad2fdebe582
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-12 13:05:03 +0800

    Delete dev directory

--- dev/demo/api/onDemo.js
+++
@@ -1,1 +0,0 @@
-async function demo(params, context) { return { "statusCode": 200, "body": "222Hello from auto Lambda!" }; } async function onDemo_default(event) { return await demo(event, __buildContext(event)); } export {  onDemo_default as default };
