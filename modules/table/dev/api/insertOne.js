dbfed159333434168f95172682a90d02ba01df77
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-12 19:37:17 +0800

    commit

--- modules/table/dev/api/insertOne.js
+++ modules/table/dev/api/insertOne.js
@@ -6,7 +6,7 @@ async function insertOne(params, context) {
 
 // build/modules/table/api/insertOne.ts
 async function insertOne_default(event) {
-  return await insertOne(event, __buildContext(event));
+  return await insertOne(event.data, __buildContext(event));
 }
 export {
   insertOne_default as default
