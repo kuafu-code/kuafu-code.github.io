ab933cd2f7e7c9e709ec73d663aa358dfd52ba82
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-12 13:21:08 +0800

    commit

---
+++ modules/demo/dev/api/onDemo/code.js
@@ -0,0 +1,1 @@
+import x from x;async function demo(params, context) { return { "statusCode": 200, "body": "222Hello import from auto Lambda!" }; } async function onDemo_default(event) { return await demo(event, __buildContext(event)); } export {  onDemo_default as default };
