3a319aac3ce3de218c773edac95101bfdf521198
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-12 19:33:16 +0800

    commit

---
+++ modules/table/dev/api/insertOne.js
@@ -0,0 +1,13 @@
+// ../open-kuafu-system/src/table/action/table.ts
+async function insertOne(params, context) {
+  console.log("params", params);
+  console.log("context", context);
+}
+
+// build/modules/table/api/insertOne.ts
+async function insertOne_default(event) {
+  return await insertOne(event, __buildContext(event));
+}
+export {
+  insertOne_default as default
+};
