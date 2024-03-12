d2e48f09d1048025fe37f7216ebc1fcb26bc8d17
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-12 20:12:02 +0800

    commit

--- modules/table/dev/api/insertOne.js
+++ modules/table/dev/api/insertOne.js
@@ -1,7 +1,30 @@
+// ../open-kuafu-system/src/sys/abs/config.ts
+var AWS_REGION = "us-east-1";
+
 // ../open-kuafu-system/src/table/action/table.ts
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 async function insertOne(params, context) {
   console.log("params", params);
   console.log("context", context);
+  const dbclient = new DynamoDBClient({ region: AWS_REGION });
+  const paramsTemp = {
+    TableName: "YourTableName",
+    // 把YourTableName替换为你的表名
+    Item: {
+      "column1": { S: "value1" },
+      // 把column1和value1替换为你需要添加的属性和值
+      "column2": { N: "111" }
+      // 把column2和value2替换为你需要添加的属性和值
+    }
+  };
+  try {
+    await dbclient.send(new PutItemCommand(paramsTemp));
+    console.log("Data inserted successfully");
+    return { code: 1 };
+  } catch (error) {
+    console.error("An error occurred while inserting data", error);
+    return { code: 2 };
+  }
 }
 
 // build/modules/table/api/insertOne.ts
