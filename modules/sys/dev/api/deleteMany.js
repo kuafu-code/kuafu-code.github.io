f3212139a7350574ce675d461ce991e6b92404f1
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-15 22:00:51 +0800

    commit

---
+++ modules/sys/dev/api/deleteMany.js
@@ -0,0 +1,10 @@
+// ../../open-kuafu-system/src/sys/action/table.ts
+async function deleteMany(params) {
+  return await TableActions.deleteMany(params);
+}
+
+// ../build/modules/sys/api/deleteMany.ts
+var deleteMany_default = deleteMany;
+export {
+  deleteMany_default as default
+};
