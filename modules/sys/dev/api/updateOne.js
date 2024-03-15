546d1911f2c176c9ab2ad8c449c7ab0d97c8f60f
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-15 22:00:59 +0800

    commit

---
+++ modules/sys/dev/api/updateOne.js
@@ -0,0 +1,10 @@
+// ../../open-kuafu-system/src/sys/action/table.ts
+async function updateOne(params) {
+  return await TableActions.updateOne(params);
+}
+
+// ../build/modules/sys/api/updateOne.ts
+var updateOne_default = updateOne;
+export {
+  updateOne_default as default
+};
