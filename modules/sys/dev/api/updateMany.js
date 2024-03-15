994a4864c70bf3afdaee6afadb8600a74aab7efd
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-15 22:00:57 +0800

    commit

---
+++ modules/sys/dev/api/updateMany.js
@@ -0,0 +1,10 @@
+// ../../open-kuafu-system/src/sys/action/table.ts
+async function updateMany(params) {
+  return await TableActions.updateMany(params);
+}
+
+// ../build/modules/sys/api/updateMany.ts
+var updateMany_default = updateMany;
+export {
+  updateMany_default as default
+};
