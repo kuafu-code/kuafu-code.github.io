8ddef717c7808fbd07d3caf035fdf6f8535ad372
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-15 22:00:53 +0800

    commit

---
+++ modules/sys/dev/api/deleteOne.js
@@ -0,0 +1,10 @@
+// ../../open-kuafu-system/src/sys/action/table.ts
+async function deleteOne(params) {
+  return await TableActions.deleteOne(params);
+}
+
+// ../build/modules/sys/api/deleteOne.ts
+var deleteOne_default = deleteOne;
+export {
+  deleteOne_default as default
+};
