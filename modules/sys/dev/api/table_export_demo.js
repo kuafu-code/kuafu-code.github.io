756222f8ed1aca081258ca622ce9455ee756b4c3
kuafu-code <159217449+kuafu-code@users.noreply.github.com>
2024-03-16 19:56:05 +0800

    commit

--- modules/sys/dev/api/table_export_demo.js
+++ modules/sys/dev/api/table_export_demo.js
@@ -1,1 +1,7 @@
-{"id":"table_export_demo","columns":{"id":{"key":true,"type":"string"},"num":{"type":"number"},"time":{"type":"number"}}}
+
+
+export default async function(params){
+
+  return await onTableRequest({"id":"table_export_demo","columns":{"id":{"key":true,"type":"string"},"num":{"type":"number"},"time":{"type":"number"}}}, params);
+
+}
